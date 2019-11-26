9dd5df25bbc38a08d16d4b4809fa00750e9e9057
rubenpixelpro <38692309+rubenpixelpro@users.noreply.github.com>
2019-11-26 15:51:27 +0100

    Video 3.1

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,14 +7,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function homepage() {
-        return $this->render('index.html.twig', 'title');
+    public function homepage(Environment $twig) {
+        $html = $twig->render('index.html.twig',['title' => 'Hotel Pixelpro']);
+
+        return new Response($html);
     }
 
 }
